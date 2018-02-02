271e43997c2fbd61409306de011ef06605044a04
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-02-02 08:48:21 +0100

    proofed csharp demo code with test

--- code/FPDemo/DemoTest/UnitTest1.cs
+++ code/FPDemo/DemoTest/UnitTest1.cs
@@ -9,14 +9,14 @@ namespace DemoTest
         [Fact]
         public void Test1()
         {
-            Func<int, bool> isLargerThanFive = x => x > 5;
-            Func<int, bool> isSmallerThenTen = x => x < 10;
+            // Func<int, bool> isLargerThanFive = x => x > 5;
+            // Func<int, bool> isSmallerThenTen = x => x < 10;
 
-            Func<int, bool> isBetweenFiveAndTen = x => 
-                isLargerThanFive(x) && isSmallerThenTen(x);
+            // Func<int, bool> isBetweenFiveAndTen = x => 
+            //     isLargerThanFive(x) && isSmallerThenTen(x);
 
-            isBetweenFiveAndTen(7)
-                .Should().BeTrue();
+            // isBetweenFiveAndTen(7)
+            //     .Should().BeTrue();
 
 
             Func<int, bool> IsDivisibleBy(int divisor) => num => num % divisor == 0;
